12352a9dd8a1a80a71cca71abc26f33b488cb5e2
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-09-04 00:06:06 +0530

    Creating Singly Linked List

---
+++ SinglyLinkedList_06.java
@@ -0,0 +1,22 @@
+public class SinglyLinkedList_06{
+    private ListNode head;
+    private static class ListNode{
+        private int data; // Generic type data
+        private ListNode next;
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args){
+        SinglyLinkedList_06 sll = new SinglyLinkedList_06();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+        // connecting them together
+        sll.head.next = second;
+        second.next = third;
+        third.next = fourth;
+    }
+}
